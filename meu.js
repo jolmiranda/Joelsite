d265db0ce253ffbe005b483bb250cc32c0aa985d
Joel Miranda <120034205+jolmiranda@users.noreply.github.com>
2023-04-05 16:18:35 -0300

    agora sim

--- meu.js
+++ meu.js
@@ -5,7 +5,7 @@ senha = window.prompt(`Ok ${usu}. Digite a senha. senha: 12345`)
 
 if (senha!=1234){
     window.alert("OPS! SENHA INCORRETA! TENTE 1234,riririri")
-    for (i in senha1!=12234){
+    for (i in senha1!=1234){
         senha1 = window.prompt("Digite a senha:")
     }
     
